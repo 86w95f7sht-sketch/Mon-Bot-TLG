c28a95caadfe86567e9fcb0ed8bb28a1709f91f8
86w95f7sht-sketch <86w95f7sht@privaterelay.appleid.com>
2026-06-27 12:52:41 +0200

    Create Confit.py

---
+++ Confit.py
@@ -0,0 +1,12 @@
+TOKEN = "TON_TOKEN_ICI"
+STARTING_BALANCE = 0
+CASHBACK_PERCENT = 5
+LEVELS = {
+    "🟤 Fer":       0,
+    "⚪ Bronze":    500,
+    "⚪ Argent":    2000,
+    "🟡 Or":       5000,
+    "💎 Platine":   15000,
+    "👑 Diamant":   50000,
+    "🔥 Légende":   100000
+}
